ab0b410f395d0197f8fe137b5232314ec9c7a94d
Tomás Claudeth Sanimbo <tomassanimbo@gmail.com>
2022-06-09 21:59:48 +0100

    page empresa add

--- pages/empresas/index.js
+++ pages/empresas/index.js
@@ -4,8 +4,21 @@ export default function Empresas(){
   return(
 
     <>
+      <section className="busca padding">
+      <div className="overlay"></div>
+        <div className="min-container">
+          <h1 className="title">
+            Econtre uma empresa para investir
+          </h1>
 
+          <form action="" className='form-busca'>
+              <div className="form-control">
+                <input type="search" name="" id="" placeholder='me ache por favor...' />
+              </div>
+          </form>
+        </div>
 
+      </section>
      
     </>
   )
