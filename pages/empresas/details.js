ad1646fd538baf5f3055885a8da12d82221653b7
Tomás Claudeth Sanimbo <tomassanimbo@gmail.com>
2022-06-10 16:47:56 +0100

    Novas section add

--- pages/empresas/details.js
+++ pages/empresas/details.js
@@ -9,21 +9,49 @@ export default function Details(){
       <section className="detalhes-company padding">
         <div className="container">
             <nav className="informar">
-              <Link href="/empresas" ><div className="back"><i className="fa-solid fa-arrow-left-long"></i>Back</div></Link>
-              <i className="fa-regular fa-heart favorite-1"></i>
+              <Link href="/empresas" ><div className="back"><i className="back-arrow fa-solid fa-arrow-left-long"></i>Back</div></Link>
+              <i className="heart fa-regular fa-heart favorite-1"></i>
 
             </nav>
-            <div className="information">
+            <div className="name-company">
             <h1 className="title-name" >Minhas informacoes</h1>
             <p>dados do IPO</p>
             </div>
 
             <section className="details-company">
               <div className="image">
-              <Image src="/image/HERO.jpg" width={300} height={250} alt="" />
+              <Image layout='fill' width={500} height={500} className="image-foto" src="/image/HERO.jpg"  alt="" />
+              </div>
+              <div className="number-invest">
+
+                <h1>25</h1>
+                {/* <h2>Investidores</h2> */}
+                <h2>Investidores</h2>
               </div>
               <div className="info-company">
                 <h1>Minhas information</h1>
+                <div className="top">
+                  <div className="dados-semana">
+                    <ul>
+                      <li>semana</li>
+                      <li>dia</li>
+                      <li>mes</li>
+                      <li>ano</li>
+                      <li>Tempo na bolsa</li>
+                    </ul>
+                  </div>
+                  <div className="dados-day">
+                  <ul>
+                      <li>Quinta feira</li>
+                      <li>26</li>
+                      <li>setembro</li>
+                      <li>22</li>
+                      <li></li>
+                      <li></li>
+                      <li>5</li>
+                    </ul>
+                  </div>
+                </div>
               </div>
 
               <div className="text-company">
@@ -32,9 +60,12 @@ export default function Details(){
                 </p>
               </div>
               <div className="button-invest">
-                <button>invest</button>
+                <button className="btn-busca btn-primary" >invest</button>
               </div>
             </section>
+
+
+            
         </div>
       </section>
     </>
